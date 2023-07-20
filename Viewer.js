9e93f117fa17de61647d74dfc791f282c9b6ac24
Dave MacFarlane <driusan@mcin.ca>
2023-07-20 15:48:56 -0400

    Render something

--- Viewer.js
+++ Viewer.js
@@ -13,15 +13,9 @@ export class Viewer extends React.Component {
             return;
         }
         let dv;
-        if (props.headers.datatype == "float32") {
-            dv = new Float32Array(this.props.rawData);
-            console.log("new dataview");
-        } else {
-            console.log("unhandled datatype");
-        }
         console.log('setting state');
         this.state = {
-            dataview: dv,
+            dataview: new DataView(props.rawData),
         };
     }
     render() {
@@ -31,7 +25,8 @@ export class Viewer extends React.Component {
         if (!this.props.rawData){
             return <View><Text>Loading raw data..</Text></View>
         }
-        console.log('rendering', this.state);
+        console.log('rendering');///, this.state);
+        this.drawFrame();
         return (
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
                 <View style={{width: 350, height: 400, backgroundColor: 'pink'}}>
@@ -54,22 +49,17 @@ export class Viewer extends React.Component {
     arrayValue = (x, y, z) => {
         // console.log('this',this);
         if (!this.state.dataview) {
-            console.warn('No dataview', this.state);
             return;
         }
         if (!this.props.headers.xspace) {
             console.warn('No xspace');
             return;
         }
-        const idx = arrayIndex(x, y, z);
-        return this.state.dataview[idx];
+        const idx = this.arrayIndex(x, y, z);
+        return this.state.dataview.getFloat32(idx);
     }
 
     drawFrame = () => {
-        if (!this.state.dataview) {
-            console.log('No dataview');
-            return;
-        }
         if (!this.props.headers.xspace) {
             console.log('no xspace', typeof(this.props.headers), this.props.headers);
             return;
@@ -82,23 +72,82 @@ export class Viewer extends React.Component {
                 this.drawPixel(x, y);
             }
         }
+
+        console.log('flushing');
+        if (!this.state.ctx) {
+            console.log('no ctx');
+        } else {
+            this.state.ctx.flush();
+            this.state.gl.endFrameEXP();
+        }
+        console.log('flushed');
+      //gl.endFrameEXP();
+    }
+
+    scale(val, min, max) {
+        if (!min || !max) {
+            return;
+            throw new Error('No min or max');
+        }
+        const range = max-min;
+        return (val-min) / range;
     }
 
     drawPixel = (x, y) => {
-        console.log('x, y = ?', x, y, this.arrayValue(x, y, 100));
+        const val = this.arrayValue(x, y, 100);
+        if (!val || !this.state.minIntensity || !this.state.maxIntensity) {
+            return;
+        }
+        if (!this.state.ctx) {
+            return;
+        }
+        // console.log(this.state.ctx, this.state.ctx.gl.drawingBufferWidth, this.props.headers.xspace.space_length);
+        const ctx = this.state.ctx;
+        const pixelXSize = ctx.gl.drawingBufferWidth / this.props.headers.xspace.space_length;
+        const pixelYSize = ctx.gl.drawingBufferHeight / this.props.headers.yspace.space_length;
+
+        const valRGB = val * 255;
+        ctx.fillStyle = "rgba(" + valRGB + ", " + valRGB + "," + valRGB + ", 1)";
+        // console.log(ctx.fillStyle);
+        // console.log(pixelXSize, pixelYSize);
+        ctx.fillRect(x*pixelXSize, y*pixelYSize, pixelXSize, pixelYSize);
+        //console.log('x, y = ?', x, y, this.scale(val, this.state.minIntensity, this.state.maxIntensity));
     }
     onContextCreate = (gl) => {
       console.log('on context create');
       const ctx = new Expo2DContext(gl);
-      this.setState({'ctx': ctx});
-      console.log('draw frame');
-      this.drawFrame();
+      if (this.props.headers.datatype == "float32") {
+            //dv = new Float32Array(this.props.rawData);
+            console.log("new dataview2");
+            const dv = new DataView(this.props.rawData);
+            let min = null;
+            let max = null;
+            for(let i = 0; i < this.props.rawData.byteLength; i += 4) {
+                const val = dv.getFloat32(i);
+                if (min == null || val < min) {
+                    min = val;
+                }
+                if (max == null || val > max) {
+                    max = val;
+                }
+            }
+            this.setState({ctx: ctx, dataview: new DataView(this.props.rawData), minIntensity: min, maxIntensity: max, gl: gl});
+            console.log('draw frame');
+            this.drawFrame();
+        } else {
+            return;
+            console.warn("unhandled datatype");
+        }
+      // this.setState({'ctx': ctx});
+      /*
+      console.log('draw square');
       ctx.fillStyle = "rgba(255, 255, 0, 1)";
       ctx.fillRect(0, 0, 10, 10);
       ctx.fillRect(30, 30, 10, 10);
       ctx.fillRect(767, 767, 10, 10);
       ctx.flush();
       gl.endFrameEXP();
+      */
       return;
     }
 }
