0d7a8e55ed06887fca11b949f2cb338ec7d699fe
Dave MacFarlane <driusan@mcin.ca>
2023-07-21 10:22:25 -0400

    use opengl instead of expo2d canvas

--- Viewer.js
+++ Viewer.js
@@ -29,8 +29,30 @@ export class Viewer extends React.Component {
 
     handleSliderChange = (newValue) => {
         this.setState({ sliderValue: newValue });
-        this.drawFrame();
+        this.drawPanel();
     };
+
+    drawPanel = () => {
+      const xsize = this.props.headers.xspace.space_length;
+      const ysize = this.props.headers.yspace.space_length;
+      const intensities = [];
+      for (let x = 0; x < xsize; x++) {
+        for(let y = 0; y < ysize; y++) {
+          intensities.push(this.arrayValue(x, y, this.state.sliderValue));
+        }
+      }
+
+      var colorUniformLocation = this.state.colorUniformLocation; 
+      for(let i = 0; i < xsize*ysize; i++) {
+          const intensity = intensities[i]; 
+          const val = (intensity-this.state.data.min) / (this.state.data.max-this.state.data.min);
+          this.state.gl.uniform4f(colorUniformLocation, val, val, val, 1);
+
+          this.state.gl.drawArrays(this.state.gl.POINTS, i, 1);
+      }
+      this.state.gl.flush();
+      this.state.gl.endFrameEXP();
+    }
 
     render() {
         if( !this.props.headers) {
@@ -76,12 +98,11 @@ export class Viewer extends React.Component {
         return y + 
             zSize * z + 
             zSize * x * ySize
-        //return y + zSize * (z  + (x * (xSize)));
-
     }
 
     arrayValue = (x, y, z) => {
         if (!this.state.dataview) {
+            // console.log('No dataview');
             return;
         }
         if (!this.props.headers.xspace) {
@@ -129,71 +150,148 @@ export class Viewer extends React.Component {
         return (val-min) / range;
     }
 
-    drawPixel = (x, y) => {
-        const val = this.arrayValue(x, y, this.state.sliderValue);
-        if (typeof(val) == "undefined" || this.state.minIntensity === null || this.state.maxIntensity === null) {
-            //console.log(val, this.state.minIntensity, this.state.maxIntensity);
-            return;
+    preprocess = (rawdata) => {
+        const dv = new DataView(rawdata);
+        const floatArray = new Float32Array(this.props.rawData.byteLength / 4);
+        let min = null;
+        let max = null;
+        for(let i = 0; i < this.props.rawData.byteLength; i += 4) {
+          const val = dv.getFloat32(i, true);
+          if (min == null || val < min) {
+            min = val;
+          }
+          if (max == null || val > max) {
+            max = val;
+          }
+          floatArray[i / 4] = val;
         }
-        if (!this.state.ctx) {
-            return;
+        console.log('min, max', min, max, floatArray.byteLength);
+        return {
+            min: min,
+            max: max,
+            floats: floatArray,
         }
-        // console.log(this.state.ctx, this.state.ctx.gl.drawingBufferWidth, this.props.headers.xspace.space_length);
-        const ctx = this.state.ctx;
-        const pixelXSize = ctx.gl.drawingBufferWidth / this.props.headers.xspace.space_length;
-        const pixelYSize = ctx.gl.drawingBufferHeight / this.props.headers.yspace.space_length;
-
-        
-         // test data goes 0-546, hack: divided by 2 to get close to
-         // 0-256
-        // const valRGB = val / 2; 
-        const valRGB = this.scale(val)*256;
-        ctx.fillStyle = "rgba(" + valRGB + ", " + valRGB + "," + valRGB + ", 1)";
-        // ctx.fillStyle = "rgba(128, 128, 128, 1)";
-        // console.log(ctx.fillStyle);
-        // console.log(pixelXSize, pixelYSize);
-        // console.log(ctx.gl.drawingBufferWidth, this.props.headers.xspace.space_length, (ctx.gl.drawingBufferWidth / this.props.headers.xspace.space_length), 'fillrect', x*pixelXSize, y*pixelYSize, pixelXSize, pixelYSize);
-
-        // console.log('filling', x, y);
-        ctx.fillRect(x*pixelXSize, y*pixelYSize, pixelXSize, pixelYSize);
-        // console.log('x, y = ?', x, y, this.scale(val, this.state.minIntensity, this.state.maxIntensity));
     }
     onContextCreate = (gl) => {
-      console.log('on context create');
-      const ctx = new Expo2DContext(gl);
-      if (this.props.headers.datatype == "float32") {
-            //dv = new Float32Array(this.props.rawData);
-            console.log("new dataview2");
-            const dv = new DataView(this.props.rawData);
-            let min = null;
-            let max = null;
-            for(let i = 0; i < this.props.rawData.byteLength; i += 4) {
-                const val = dv.getFloat32(i, true);
-                if (min == null || val < min) {
-                    min = val;
-                }
-                if (max == null || val > max) {
-                    max = val;
-                }
-            }
-            console.log('min, max', min, max);
-            this.setState({ctx: ctx, dataview: new DataView(this.props.rawData), minIntensity: min, maxIntensity: max, gl: gl});
+        const data = this.preprocess(this.props.rawData);
+        this.setState({data: data, gl: gl, dataview: new DataView(this.props.rawData)});
+
+            // this.setState({ctx: ctx, dataview: new DataView(this.props.rawData), minIntensity: min, maxIntensity: max, gl: gl});
             console.log('draw frame');
-            this.drawFrame();
-        } else {
-            return;
-            console.warn("unhandled datatype");
-        }
-      // this.setState({'ctx': ctx});
-      /*
-      console.log('draw square');
-      ctx.fillStyle = "rgba(255, 255, 0, 1)";
-      ctx.fillRect(0, 0, 10, 10);
-      ctx.fillRect(30, 30, 10, 10);
-      ctx.fillRect(767, 767, 10, 10);
-      ctx.flush();
-      gl.endFrameEXP();
-      */
-      return;
+      console.log('on context create');
+  gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+  gl.clearColor(0, 1, 1, 1);
+
+  // Create vertex shader (shape & position)
+  const vert = gl.createShader(gl.VERTEX_SHADER);
+  // FIXME: Fix gl_PointSize, should be calculated based on drawingBuferWidth/Height and xspace/yspace length
+  gl.shaderSource(
+    vert,
+    `
+    attribute vec2 a_pos;
+    uniform vec2 u_resolution;
+    uniform float u_pixelsize;
+    void main(void) {
+      vec2 normalize_to_one = a_pos / u_resolution;
+      vec2 normalize_to_two = normalize_to_one * 2.0;
+      vec2 normalize_to_clipspace = normalize_to_two - 1.0;
+
+      gl_Position = vec4(normalize_to_clipspace, 0, 1);
+      gl_PointSize = 6.0; 
+    }
+  `
+  );
+      // gl_Position = vec4(normalize_to_clipspace, 0, 1);
+  gl.compileShader(vert);
+
+  // Create fragment shader (color)
+  const frag = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(
+    frag,
+    `
+    precision mediump float;
+
+    uniform vec4 u_color;
+    void main(void) {
+      gl_FragColor = u_color;
+    }
+  `
+  );
+  gl.compileShader(frag);
+
+  // Link together into a program
+  const program = gl.createProgram();
+
+  let positionAttributeLocation = gl.getAttribLocation(program, "a_pos");
+
+  
+  let positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  let positions = [];
+
+  const xsize = this.props.headers.xspace.space_length;
+  const ysize = this.props.headers.yspace.space_length;
+  const intensities = [];
+  for (let x = 0; x < xsize; x++) {
+      for(let y = 0; y < ysize; y++) {
+          positions.push(x);
+          positions.push(y);
+      }
+  }
+  let resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
+  let pixelSizeUniformLocation = gl.getUniformLocation(program, "u_pixelsize");
+  //gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+  //console.log('u_resolition', xsize, ysize);
+  
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, data.floats);
+  gl.bufferData(gl.ARRAY_BUFFER, data.floats, gl.STATIC_DRAW);
+
+
+  gl.attachShader(program, vert);
+  gl.attachShader(program, frag);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+  gl.uniform2f(resolutionUniformLocation, xsize, ysize);
+  console.log('pointuniform', pixelSizeUniformLocation, resolutionUniformLocation);
+  gl.uniform1f(pixelSizeUniformLocation, 10.0);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+ 
+ // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+ var size = 2;          // 2 components per iteration
+ var type = gl.FLOAT;   // the data is 32bit floats
+ var normalize = false; // don't normalize the data
+ var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+ var offset = 0;        // start at the beginning of the buffer
+ gl.vertexAttribPointer(
+     positionAttributeLocation, size, type, normalize, stride, offset)
+
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  var colorUniformLocation = gl.getUniformLocation(program, "u_color");
+  this.setState({colorUniformLocation: colorUniformLocation});
+
+  for(let i = 0; i < xsize*ysize; i++) {
+      const intensity = intensities[i];
+      if (!intensity) {
+          continue;
+      }
+      //const val = (intensity-data.min) / (data.max-data.min);
+      const val = 0.5;
+
+      // console.log('val', i, intensity, data.min, data.max, val);
+      gl.uniform4f(colorUniformLocation, val, val, val, 1);
+
+      gl.drawArrays(gl.POINTS, i, 1);
+  }
+  console.log('drew');
+
+  gl.flush();
+  gl.endFrameEXP();
+return;
     }
 }
